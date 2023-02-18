e38fe5f77ce66466fa7f6c1abbbc8ab71bcdf0d1
Mohamed Amarcha <simoamarcha@icloud.com>
2023-02-18 01:22:15 +0100

    navbar not responsive

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css'
+
+export default function Home(){
+    return (
+        <div>
+            <h1>Creative</h1>
+            <h1>Developer</h1>
+            <img src="" alt="" />
+            <h1>Ozawa</h1>
+        </div>
+    )
+}
---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Navbar.css'
+import './button.css'
+
+export default function Navbar(){
+    return(
+        <nav>
+            <h1 className='nav-item'>OZAWAKYO ©</h1>
+            <div className='nav-right'>
+                <h1 className='nav-item nav-right-item'>Home</h1>
+                <h1 className='nav-item nav-right-item'>Projects</h1>
+                <h1 className='nav-item nav-right-item'>Music</h1>
+                <button className='nav-item nav-right-item button-86'>Contact</button>
+            </div>
+        </nav>
+    )
+}
