ea3464262139883e34edab99acb02ad34f974eca
Mohamed Amarcha <simoamarcha@icloud.com>
2023-02-18 01:55:10 +0100

    same , lesser code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -10,19 +10,14 @@ export default function Navbar() {
   };
 
   return (
-    <nav>
-      <h1 className="nav-item">OZAWAKYO ©</h1>
-      <div className={`nav-right ${isOpen ? 'show' : ''}`}>
-        <h1 className="nav-item nav-right-item">Home</h1>
-        <h1 className="nav-item nav-right-item">Projects</h1>
-        <h1 className="nav-item nav-right-item">Music</h1>
-        <button className="nav-item nav-right-item button-86">Contact</button>
-      </div>
-      <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
-      </div>
-    </nav>
+        <nav>
+            <h1 className='nav-item'>OZAWAKYO ©</h1>
+            <div className='nav-right'>
+                <h1 className='nav-item nav-right-item'>Home</h1>
+                <h1 className='nav-item nav-right-item'>Projects</h1>
+                <h1 className='nav-item nav-right-item'>Music</h1>
+                <button className='nav-item nav-right-item button-86'>Contact</button>
+            </div>
+        </nav>
   );
 }
