5ac88a1e36acecfe01cdb44b68ccbe8db0de69e8
Mohamed Amarcha <simoamarcha@icloud.com>
2023-02-18 01:37:08 +0100

    2nd try , works but nothing shows

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,25 +3,25 @@ import './Navbar.css';
 import './button.css';
 
 export default function Navbar() {
-  const [showMenu, setShowMenu] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleMenuClick = () => {
-    setShowMenu(!showMenu);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
     <nav>
-      <h1 className='nav-item'>OZAWAKYO ©</h1>
-      <button className="burger-button" onClick={handleMenuClick}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <div className={showMenu ? 'nav-right show' : 'nav-right'}>
-        <h1 className='nav-item nav-right-item'>Home</h1>
-        <h1 className='nav-item nav-right-item'>Projects</h1>
-        <h1 className='nav-item nav-right-item'>Music</h1>
-        <button className='nav-item nav-right-item button-86'>Contact</button>
+      <h1 className="nav-item">OZAWAKYO ©</h1>
+      <div className={`nav-right ${isOpen ? 'show' : ''}`}>
+        <h1 className="nav-item nav-right-item">Home</h1>
+        <h1 className="nav-item nav-right-item">Projects</h1>
+        <h1 className="nav-item nav-right-item">Music</h1>
+        <button className="nav-item nav-right-item button-86">Contact</button>
+      </div>
+      <div className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="bar1"></div>
+        <div className="bar2"></div>
+        <div className="bar3"></div>
       </div>
     </nav>
   );
